c8657d18e1e6a4f77a5781eb190f72a32503d9f9
faizasaba <faizasaba298@gmail.com>
2024-12-27 22:28:02 +0500

    java 1st

---
+++ main.js
@@ -0,0 +1,86 @@
+// 1 ALERT:
+
+alert("welcome to the javascript");
+var Name=prompt("what is your name?");
+alert("hello!"+""+Name);
+
+// 2 VARIABLES FOR STRINGS:
+var firstName="faiza";
+var lastName="saba";
+var fullName= firstName+""+lastName;
+alert(fullName);
+
+// task:2
+var hobby="typing";
+alert("My favorite hobby is "+hobby+"");
+
+// 3 VARIABLE FOR NUMBERS
+var age="19";
+var yearOfBirth="2005";
+var currentYear="2024";
+var calculatedAge=currentYear-yearOfBirth;
+alert("your age is"+calculatedAge);
+
+// TASK2:
+var length="6";
+var width="10";
+var area=6*10;
+alert("The area of the rectangle is"+60);
+
+
+
+// TASK 2
+// CHANGE VARIABLE VALUES
+var age="19";
+alert("My age:"+age);
+var age="24";
+alert("My friends age:"+age);
+
+// FAMILIAR OPERATORS
+var num1=prompt("enter first no");
+var num2=prompt("enter second no");
+var sum=num1+num2;
+alert("the sum is "+sum);
+ 
+// RESULT OF MULTIPLYING
+
+var result=6*8;
+alert("6*8="+result);
+
+var remainder=17%5;
+alert(remainder);2
+
+// ADDITIONAL CHALLENGES
+
+var birthYear=prompt("enter your birthYear");
+var currentYear=prompt("enter currentYEAR");
+var age=birthYear-currentYear;
+aler("your age is :="+age);
+
+// TASK 2
+
+var number1 = prompt("enter the first number:");
+var number2 = prompt("enter the second number:");
+SUM_TOTAL = number1 + number2;
+DIFFERENCE_TOTAL = number2 - number1;
+PRODUCT_TOTAL = number1 * number2;
+QUOTIENT_TOTAL = number1 / number2;
+REMINDER_TOTAL = number1 % number2;
+alert("SUM = " + SUM_TOTAL);
+alert("DIFFERENCE = " + DIFFERENCE_TOTAL);
+alert("PRODUCT = " + PRODUCT_TOTAL);
+alert("QUOTIENT = " + QUOTIENT_TOTAL);
+alert("REMINDER = " + REMINDER_TOTAL);
+
+
+  
+// 4 LEGAL VARIABLES
+
+var Name;
+var $price;
+var _age;
+
+// ILLEGAL VARIABLES
+ var .number
+ var .saba
+ var saba.1
